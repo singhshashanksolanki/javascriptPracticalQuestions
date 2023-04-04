8e32657b9a5a591b2e4d2f082bfec34e0c4af877
singhshashanksolanki <shashanksinghsolanki1@gmail.com>
2023-04-05 00:57:31 +0530

    05-04-2023

--- question1.js
+++ question1.js
@@ -1,1 +1,15 @@
-hello
+// Printing The fibonacci Series
+function fibonacciSeries() {
+    // Prinnting 10 number of fibonacci series
+    let a = 0, b = 1;
+    console.log(a);
+    console.log(b);
+    for(let i = 0; i < 10; i++) {
+        let next = a + b
+        console.log(next);
+        a = b;
+        b = next;
+    }
+}
+
+fibonacciSeries();
